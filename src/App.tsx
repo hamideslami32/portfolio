97867990fc02479ef1c312a08f2be2ba27585a7c
Hamid Eslami <heslami32@gmail.com>
2022-08-13 00:25:07 +0430

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './app.module.scss'
+
+function App() {
+
+  return (
+    <div className={styles.app}>
+      <h1>Hamid Eslami Portfolio (Coming soon ...)</h1>
+    </div>
+  )
+}
+
+export default App
