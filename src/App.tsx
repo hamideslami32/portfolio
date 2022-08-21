b6a3c906b65e9123585b85b6361c1ee898f1c613
Hamid Eslami <heslami32@gmail.com>
2022-08-22 02:00:27 +0430

    update: add phone link email

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ function App() {
     <div className={styles.app}>
       <h1>Hamid Eslami</h1>
       <h2>Front-End Developer</h2>
-      <h3>heslami32@gmail.com</h3>
+      <h3><a href='tel:+989366182745'>+98-936-6182745</a></h3>
+      <h3><a href='mailto:heslami32@gmail.com'>heslami32@gmail.com</a></h3>
     </div>
   )
 }
