d684a62ac979260a078c8a9cdbcbea98edcce672
Hamid Eslami <heslami32@gmail.com>
2024-01-23 01:24:28 +0330

    chore: fill resume data

--- src/App.tsx
+++ src/App.tsx
@@ -25,34 +25,112 @@ const skillsContent = (
 
 const experienceData: ExperienceSectipnProps[] = [
   {
-    title: "Frontend Team Lead",
     companyName: "Risklick",
-    startDate: "2022/08/01",
-    endDate: "2023/10/01",
-    achievements: [
-      "Coach a team of three front-end developers, ensuring their job satisfaction.",
-      "Improved work efficiency by approximately 10% through the creation of Design System Components.",
-      "Established CI/CD pipelines for staging environments using GitHub Actions and Gitlab CI/CD.",
-      "Refactored a product using Vite, Typescript, and React.",
-    ],
     location: "Bern, Switzerland | remote",
-    link: "https://risklick.ch"
+    link: "https://risklick.ch",
+    positions: [
+      {
+        title: "Frontend Team Lead",
+        startDate: "2022/08/01",
+        endDate: "2023/10/01",
+        achievements: [
+          "Coach a team of three front-end developers, ensuring their job satisfaction.",
+          "Improved work efficiency by approximately 10% through the creation of Design System Components.",
+          "Established CI/CD pipelines for staging environments using GitHub Actions and Gitlab CI/CD.",
+          "Refactored a product using Vite, Typescript, and React.",
+        ],
+      },
+    ],
   },
   {
-    title: "Frontend Team Lead",
-    companyName: "3click",
-    startDate: "2021/05/01",
-    endDate: "2022/07/01",
-    achievements: [
-      "Led a team of 8 front-end developers",
-      "Achieved a 10X increase in flight ticket sales in two years.",
-      "Created the CRS admin dashboard with 150+ report pages including tables and charts.",
-      "Maintained high standards through code reviews and best practices.",
-      "Managed products with over 1 million monthly visits and 50K+ active users.",
-      "Resolved the rough-referral problem in Google Analytics with more than 98% accuracy. (Link)"
-    ],
     location: "Tehran, Iran",
-    link: "https://3click.com"
+    link: "https://3click.com",
+    companyName: "3click",
+    positions: [
+      {
+        title: "Frontend Team Lead",
+        startDate: "2021/05/01",
+        endDate: "2022/07/01",
+        achievements: [
+          "Led a team of 8 front-end developers",
+          "Achieved a 10X increase in flight ticket sales in two years.",
+          "Created the CRS admin dashboard with 150+ report pages including tables and charts.",
+          "Maintained high standards through code reviews and best practices.",
+          "Managed products with over 1 million monthly visits and 50K+ active users.",
+          "Resolved the rough-referral problem in Google Analytics with more than 98% accuracy. (Link)",
+        ],
+      },
+      {
+        title: "Frontend Engineer",
+        startDate: "2020/11/01",
+        endDate: "2021/04/01",
+        achievements: [
+          "Implemented complex sorting and filtering of 3K+ rows of data on the client side using Web workers.",
+          "Documented projects using Storybook.",
+        ],
+      },
+    ],
+  },
+  {
+    companyName: "Apro",
+    location: "Tehran, Iran",
+    positions: [
+      {
+        title: "Frontend Developer",
+        startDate: "2020/05/01",
+        endDate: "2020/10/01",
+        achievements: [
+          "Developed a B2B product connecting local travel agencies to airlines using Vue, Nuxt, and Vuetify.",
+        ],
+      },
+    ],
+  },
+  {
+    location: "Tehran, Iran",
+    companyName: "Chamedoon",
+    positions: [
+      {
+        title: "Frontend Developer",
+        startDate: "2019/05/01",
+        endDate: "2020/04/01",
+        achievements: [
+          "Optimized page load speed from 7 to about 3 seconds through image optimization, chunking files, and code splitting.",
+          "Integrated Sentry with projects to handle edge cases that caused decreased production bugs by nearly 40%.",
+        ],
+      },
+      {
+        title: "Junior Frontend Developer",
+        startDate: "2018/05/01",
+        endDate: "2019/04/01",
+        achievements: [
+          "Produced visually appealing components and animations based on UI/UX designs.",
+          "Utilized Strapi with GraphQL to streamline content updates.",
+        ],
+      },
+    ],
+  },
+  {
+    location: "Remote/On-Site",
+    companyName: "Freelancer",
+    positions: [
+      {
+        title: "Web Developer",
+        startDate: "2016/03/01",
+        endDate: "2021/12/01",
+        achievements: [
+          "Developed and maintained multiple projects using Vue, React, Node.",
+          "Mentored over 15 junior developers to enhance their skills.",
+        ],
+      },
+      {
+        title: "SEO Consultant",
+        startDate: "2018/01/01",
+        endDate: "2021/12/01",
+        achievements: [
+          "Optimized SEO for various projects, achieving high Lighthouse scores and top-ranking positions for primary keywords.",
+        ],
+      },
+    ],
   },
 ];
 
@@ -65,6 +143,46 @@ const experienceContent = (
     ))}
   </>
 );
+const projectsContent = (
+  <ul>
+    <li className="flex gap-2 items-baseline">
+      <span className="">*</span>
+      <span>
+        Darmankade, Created a mobile application using Capacitor.js to export
+        APK and PWA with a single codebase.
+      </span>
+    </li>
+  </ul>
+);
+
+interface Article {
+  title: string;
+  link: string;
+}
+
+const articlesData: Article[] = [
+  {
+    title: "Pro Tips for Clean Code in TypeScript with React!",
+    link: "https://medium.com/@heslami32/pro-tips-for-clean-code-in-typescript-with-react-a2a0740a247b",
+  },
+  {
+    title: "What is Rogue Referral and how to fix it?",
+    link: "https://salt.agency/blog/rogue-referral/",
+  },
+];
+
+const articlesContent = (
+  <ul>
+    {articlesData.map((article, index) => (
+      <a key={index} href={article.link} className="flex gap-2 items-baseline">
+        <span className="">*</span>
+        <li className="underline text-blue-500">
+          <span>{article.title}</span>
+        </li>
+      </a>
+    ))}
+  </ul>
+);
 function App() {
   return (
     <div className="max-w-cs min-h-screen mx-auto p-6">
@@ -75,12 +193,21 @@ function App() {
       <Section title="Skills">{skillsContent}</Section>
       <Divider />
       <Section title="Experiences">{experienceContent}</Section>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <Divider />
+      <Section title="Projects">{projectsContent}</Section>
+      <Divider />
+      <Section title="Articles">{articlesContent}</Section>
+      <Divider />
+      <Section title="Education">
+        Payame-Noor University | Physics associate degree | 2007 - 2010
+      </Section>
+      <Divider />
+      <Section title="Languages">
+        <div className="flex gap-2 items-baseline">
+          <span className="font-bold">English:</span>
+          <span>Fluent</span>
+        </div>
+      </Section>
     </div>
   );
 }
--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -1,49 +1,65 @@
-export interface ExperienceSectipnProps {
-  companyName: string;
-  location: string;
+interface Position {
   title: string;
   startDate: string | Date;
   endDate: string | Date | undefined;
   achievements: string[];
-  link?: string
+}
+
+export interface ExperienceSectipnProps {
+  companyName: string;
+  location: string;
+  link?: string;
+  positions: Position[];
 }
 const ExperienceSection = (props: ExperienceSectipnProps) => {
-  const { companyName, location, title, startDate, endDate, achievements, link } =
-    props;
+  const { positions, link, companyName, location } = props;
 
   const getDate = (date: string | Date) => {
     const tempDate = new Date(date).toLocaleString("default", {
       month: "long",
       year: "numeric",
     });
-    console.log({ tempDate });
     return tempDate;
   };
   return (
     <div>
       <div className="flex gap-4 items-baseline">
-        <h3 className="font-bold text-xl">{
-        link ?
-            <a className="underline" href={link}>{companyName}</a>
-        :
-        companyName}</h3>
+        <h3 className="text-2xl font-bold text-gray-600 underline">
+          {link ? (
+            <a target="_blank" href={link}>
+              {companyName}
+            </a>
+          ) : (
+            companyName
+          )}
+        </h3>
         <span className="text-sm text-gray-500">({location})</span>
       </div>
-      <div>
-        <span className="font-bold text-2xl text-blue-800">{title}</span>|{" "}
-        <span className="font-bold">
-        {`${getDate(startDate)} - ${endDate && getDate(endDate)}`}
-
-        </span>
-      </div>
-      <ul className="flex flex-col gap-3 my-2">
-        {achievements.map((achievement) => (
-          <li className="flex gap-2 items-baseline">
-            <span className="">🎯</span>
-            <span>{achievement}</span>
-          </li>
+      <div className="pl-2 pt-2">
+        {positions.map((position, index) => (
+          <>
+            <div key={index}>
+              <span className="font-extrabold text-2xl text-blue-800">
+                {position.title}
+              </span>
+              |{" "}
+              <span className="font-bold">
+                {`${getDate(position.startDate)} - ${
+                  position?.endDate && getDate(position.endDate)
+                }`}
+              </span>
+            </div>
+            <ul className="flex flex-col gap-3 my-2">
+              {position.achievements.map((achievement) => (
+                <li className="flex gap-2 items-baseline">
+                  <span className="">*</span>
+                  <span>{achievement}</span>
+                </li>
+              ))}
+            </ul>
+          </>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
